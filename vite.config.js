0532e580d6f750aee61bc141db1b5f3bef26a91b
Hamza Younis <hamzayounis106@gmail.com>
2024-07-20 02:39:32 +0500

    chore: Update Vite configuration to use React plugin and fix API endpoint proxy

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
-        target: "https://gupshup-server-3065f391ce53.herokuapp.com/",
-        changeOrigin: true,
-      },
+      // "/api": "http://localhost:8080/",
+      '/api': 'https://gupshup-server-3065f391ce53.herokuapp.com/'
     },
   },
 });
